c0b99e2dbf24f29a7fb818a91c73874d57fb48f3
Merijn Hendriks <innohurrytocode@gmail.com>
2020-02-11 16:56:32 +0100

    [WIP] work dump

--- src/responses/repair.js
+++ src/responses/repair.js
@@ -1,8 +1,3 @@
 "use strict";
 
-function handleRepair(url, info, sessionID) {
-    return repair_f.main(info);
-}
-
-router.addStaticRoute("/client/repair/exec", handleRepair);
 item_f.itemServer.addRoute("Repair", repair_f.main);
