e3b7f88541c28e81e87d4014a02c33b336746d5f
Merijn Hendriks <innohurrytocode@gmail.com>
2020-02-21 12:33:46 +0100

    Fix /client/customization response

--- src/responses/customization.js
+++ src/responses/customization.js
@@ -1,14 +1,14 @@
 "use strict";
 
 function getCustomization(url, info, sessionID) {
-    return json.stringify(getCustomization());
+    return json.stringify(customization_f.getCustomization());
 }
 
 function getCustomizationStorage(url, info, sessionID) {
     return json.read(customization_f.getPath(sessionID));
 }
 
-router.addStaticRoute("/client/customization", customization_f.getCustomization);
+router.addStaticRoute("/client/customization", getCustomization);
 router.addStaticRoute("/client/trading/customization/storage", getCustomizationStorage);
 item_f.itemServer.addRoute("CustomizationWear", customization_f.wearClothing);
 item_f.itemServer.addRoute("CustomizationBuy", customization_f.buyClothing);
