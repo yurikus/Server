88e429552dee7ade4916d17a7dc41e0a06d83281
emperor06 <github@draxar.com>
2020-02-21 23:55:59 +0100

    Temporary fix for startup issue.
    helpfunc tries to create _lookup using global.template, which is not
    available yet.
    
    Temporary fixed by making _lookup a global variable created after templates.
    
    That's probably not how it will be when definitely fixed.

--- src/classes/staticdata.js
+++ src/classes/staticdata.js
@@ -4,6 +4,7 @@ function initialize() {
     global.items = json.parse(json.read(db.user.cache.items));
     global.globals = json.parse(json.read(db.globals));
     global.templates = json.parse(json.read(db.user.cache.templates));
+    global.tplLookup = itm_hf.createLookup();
 }
 
 module.exports.initialize = initialize;
