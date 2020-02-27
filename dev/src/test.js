a076ac65160edfe61724bf86e995feee4aa8feb0
TheNullBoolean <61397019+TheNullBoolean@users.noreply.github.com>
2020-02-26 19:18:23 -0500

    Rewrote testing to use Mocha

--- dev/src/test.js
+++ dev/src/test.js
@@ -1,16 +1,312 @@
-﻿process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // ignore selfsigned ssl certificate
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // ignore selfsigned ssl certificate
 
 const fs = require('fs');
 const zlib = require('zlib');
 const https = require('https');
-let integer = 0;
-let gameVersion = '0.12.1.5208';
+const should = require('chai').should();
+const expect = require('chai').expect;
 
-function readJson(file) { //read json file with deleting all tabulators and new lines
-    return (fs.readFileSync(file, 'utf8')).replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '');
+// Global config stuff
+let settings = JSON.parse(readJson(__dirname + "/../../user/server.config.json"));
+const url = settings.server.ip;
+const port = 443;
+const gameVersion = '0.12.3.5985';
+
+let integer = 0;
+
+describe('Client', function() {
+
+  describe('/client/languages', function() {
+    it('should retrieve languages', async function() {
+      const path = '/client/languages';
+      const data = '{"crc":0}';
+
+      let res = await send(url, port, path, data);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/menu/locale/en', function() {
+    it('should load menu en locale', async function() {
+      const path = '/client/menu/locale/en';
+      const data = '{"crc":0}';
+
+      let res = await send(url, port, path, data);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/game/version/validate', function() {
+    it('should validate game version', async function() {
+      const path = '/client/game/version/validate';
+      const data = `{"version":{"major":"${gameVersion}","minor":"live","game":"live","backend":"6","taxonomy":"341"},"develop":true}`;
+
+      let res = await send(url, port, path, data);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/launcher/profile/login', function() {
+    it('should login', async function() {
+      const path = '/launcher/profile/login';
+      const data = '{"token" : "eyJlbWFpbCI6InVzZXJAamV0LmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQiLCJ0b2dnbGUiOnRydWUsInRpbWVzdGFtcCI6MTU3NzU3MjgzOX0==" }';
+
+      let res = await send(url, port, path, data);
+      const jsonData = parseData(res);
+
+      Object.keys(jsonData).should.include('profileId');
+    });
+  });
+
+  describe('/client/game/keepalive', function() {
+    it('should keepalive', async function() {
+      const path = '/client/game/keepalive';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/items', function() {
+    it('should get items', async function() {
+      const path = '/client/items';
+      const data = '{"crc":1074351527}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/globals', function() {
+    it('should load globals', async function() {
+      const path = '/client/globals';
+      const data = '{"crc":0}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/game/profile/list', function() {
+    it('should load a profile list', async function() {
+      const path = '/client/game/profile/list';
+      const data = '{}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+      jsonData.data.length.should.equal(2); // Should have 2 profiles
+    });
+  });
+
+  describe('/client/locations', function() {
+    it('should load locations', async function() {
+      const path = '/client/locations';
+      const data = '{"crc":850408639}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/weather', function() {
+    it('should load weather', async function() {
+      const path = '/client/weather';
+      const data = '{}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/locale/en', function() {
+    it('should load en locale', async function() {
+      const path = '/client/locale/en';
+      const data = '{"crc":2863236201}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/game/profile/select', function() {
+    it('should select game profile', async function() {
+      const path = '/client/game/profile/select';
+      const data = '{"uid":"5c71b934354682353958e984"}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/profile/status', function() {
+    it('should get profile status', async function() {
+      const path = '/client/profile/status';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/handbook/templates', function() {
+    it('should load handbook templates', async function() {
+      const path = '/client/handbook/templates';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/quest/list', function() {
+    it('should load quests', async function() {
+      const path = '/client/quest/list';
+      const data = '{"completed":true}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/notifier/channel/create', function() {
+    it('should create a notifier channel', async function() {
+      const path = '/client/notifier/channel/create';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/mail/dialog/list', function() {
+    it('should get mail list', async function() {
+      const path = '/client/mail/dialog/list';
+      const data = '{"limit":30,"offset":0}';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/friend/list', function() {
+    it('should get friend list', async function() {
+      const path = '/client/friend/list';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/friend/request/list/inbox', function() {
+    it('should get all friend requests incoming', async function() {
+      const path = '/client/friend/request/list/inbox';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/friend/request/list/outbox', function() {
+    it('should get all friend requests outgoing', async function() {
+      const path = '/client/friend/request/list/outbox';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/server/list', function() {
+    it('should get the server list', async function() {
+      const path = '/client/server/list';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/client/trading/api/getTradersList', function() {
+    it('should get a list of traders', async function() {
+      const path = '/client/trading/api/getTradersList';
+      const data = '';
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+  describe('/OfflineRaidSave', function() {
+    it('should save offline raid data', async function() {
+      const path = '/OfflineRaidSave';
+      const data = readJson(__dirname + "/../db/offlineRaidprofile.json");
+
+      let res = await send(url, port, path, data, 1);
+      const jsonData = parseData(res);
+
+      jsonData.err.should.equal(0);
+    });
+  });
+
+});
+
+/*
+ *
+ *  Helper Functions, used to make requests, etc
+ *
+ */
+
+function parseData(data) {
+  const body = zlib.inflateSync(data);
+  return JSON.parse((body !== typeof "undefined" && body !== null && body !== "") ? body.toString() : '{}');
 }
 
-function send(url, _port = 443, path, data, type = "POST"){
+function send(url, _port = 443, path, data, session, type = "POST"){
 	return new Promise ((resolve, reject) => {
 		const options = { // options for https data it must stay like this
 		  hostname: url,
@@ -22,9 +318,14 @@ function send(url, _port = 443, path, data, type = "POST"){
 			  'Content-Type': 		'application/json',
 			  'Accept': 			'application/json',
 			  'App-Version': 		'EFT Client ' + gameVersion,
-			  'GClient-RequestId': 	integer
+				'GClient-RequestId': 	integer,
 		  } 
 		};
+
+		// Set our session if we've got one
+		if (session) {
+			options.headers['Cookie'] = `PHPSESSID=${session}`;
+		}
 
 		integer++; // add integer number to request counting requests and also making their stupid RequestId Counter
 
@@ -58,74 +359,6 @@ function send(url, _port = 443, path, data, type = "POST"){
 	});
 }
 
-async function testServer() {
-	console.log("\nSERVER TESTER STARTING...");
-
-	let settings = JSON.parse(readJson(__dirname + "/../../user/server.config.json"));
-	const url = settings.server.ip;
-	const port = 443;
-	const path = [
-	/* 0	*/	"/",
-	/* 1	*/	"/client/languages",
-	/* 2	*/	"/client/menu/locale/en",
-	/* 3	*/	"/client/game/version/validate",
-	/* 4	*/	"/client/game/login",
-	/* 5	*/	"/client/game/keepalive",
-	/* 6	*/	"/client/items",
-	/* 7	*/	"/client/globals",
-//	/* 8	*/	"/client/game/profile/list",
-	/* 9	*/	"/client/locations",
-	/* 10	*/	"/client/weather",
-	/* 11	*/	"/client/locale/en",
-	/* 12	*/	"/client/game/profile/select",
-	/* 13	*/	"/client/profile/status",
-	/* 14	*/	"/client/handbook/templates",
-	/* 15	*/	"/client/quest/list",
-	/* 16	*/	"/client/notifier/channel/create",
-	/* 17	*/	"/client/mail/dialog/list",
-	/* 18	*/	"/client/friend/list",
-	/* 19	*/	"/client/friend/request/list/inbox",
-	/* 20	*/	"/client/friend/request/list/outbox",
-	/* 21	*/	"/client/server/list",
-	/* 22	*/	"/client/trading/api/getTradersList",
-	/* 23	*/	"/OfflineRaidSave"
-	];
-	const data = [
-	/* 0	*/	"",
-	/* 1	*/	'{"crc":0}',
-	/* 2	*/	'{"crc":0}',
-	/* 3	*/	'{"version":{"major":"' + gameVersion + '","minor":"live","game":"live","backend":"6","taxonomy":"341"},"develop":true}',
-	/* 4	*/	'{"email":"user@jet.com","pass":"5f4dcc3b5aa765d61d8327deb882cf99","version":{"major":"' + gameVersion + '","minor":"live","game":"live","backend":"6","taxonomy":"341"},"device_id":"7ae28fd31fd437b6085385aede3141ae","develop":true,"sec":1}',
-	/* 5	*/	'{}',
-	/* 6	*/	'{"crc":1074351527}',
-	/* 7	*/	'{"crc":0}',
-//	/* 8	*/	'{}',
-	/* 9	*/	'{"crc":850408639}',
-	/* 10	*/	'{}',
-	/* 11	*/	'{"crc":2863236201}',
-	/* 12	*/	'{"uid":"5c71b934354682353958e984"}',
-	/* 13	*/	'{}',
-	/* 14	*/	'{}',
-	/* 15	*/	'{"completed":true}',
-	/* 16	*/	'{}',
-	/* 17	*/	'{"limit":30,"offset":0}',
-	/* 18	*/	'{}',
-	/* 19	*/	'{}',
-	/* 20	*/	'{}',
-	/* 21	*/	'{}',
-	/* 22	*/	'{}',
-	/* 23	*/	readJson(__dirname + "/../db/offlineRaidprofile.json")
-	];
-
-	for (let i = 0; i < path.length; i++) {
-		try {
-			let res = await send(url, port, path[i], data[i]);
-			console.log("  Status of request: " + ((res.length > 0 || url != "/OfflineRaidSave") ? "[OK]" : "[BAD]"));
-			console.log("");
-		} catch (err) {
-			console.log("»SCRIPT ERROR » " + path[i], err);
-		}
-	}
+function readJson(file) { //read json file with deleting all tabulators and new lines
+  return (fs.readFileSync(file, 'utf8')).replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '');
 }
-
-testServer();
