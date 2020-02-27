560e289b7816c029adb80da9db5f477e2686d685
TheNullBoolean <61397019+TheNullBoolean@users.noreply.github.com>
2020-02-26 20:32:37 -0500

    Update setup-ci.js

--- dev/src/setup-ci.js
+++ dev/src/setup-ci.js
@@ -6,3 +6,7 @@ settings.server.httpsPort = 8080;
 fs.writeFile("../../user/server.config.json", json.stringify(settings, null, 2), function writeJSON(err) {
   if (err) return console.log(err);
 });
+
+function readJson(file) { //read json file with deleting all tabulators and new lines
+  return (fs.readFileSync(file, 'utf8')).replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '');
+}
