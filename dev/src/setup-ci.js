b0b603c5ad38785e9285703b93e3de061605ba0f
TheNullBoolean <61397019+TheNullBoolean@users.noreply.github.com>
2020-02-26 20:51:56 -0500

    Update setup-ci.js

--- dev/src/setup-ci.js
+++ dev/src/setup-ci.js
@@ -3,15 +3,12 @@ const fs = require('fs');
 let settings = JSON.parse(readJson(__dirname + "/../../user/server.config.json"));
 
 // Setup our custom https port for travis ci
-settings.server.httpsPort = 8080;
-
-console.log(settings.server.httpsPort);
+settings.server.httpPort = 8080;
+settings.server.httpsPort = 8081;
 
 fs.writeFileSync(__dirname + "/../../user/server.config.json", JSON.stringify(settings), 'utf8');
 
-console.log('Modified output...');
-console.log(settings);
-console.log(JSON.parse(readJson(__dirname + "/../../user/server.config.json")));
+console.log('Setup server config for CI');
 
 function readJson(file) { //read json file with deleting all tabulators and new lines
   return (fs.readFileSync(file, 'utf8')).replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '');
