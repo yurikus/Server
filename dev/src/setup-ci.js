5408d22062060ff904c982ea0bf32162fe660026
TheNullBoolean <61397019+TheNullBoolean@users.noreply.github.com>
2020-02-26 20:37:16 -0500

    Update setup-ci.js

--- dev/src/setup-ci.js
+++ dev/src/setup-ci.js
@@ -5,7 +5,7 @@ let settings = JSON.parse(readJson(__dirname + "/../../user/server.config.json")
 // Setup our custom https port for travis ci
 settings.server.httpsPort = 8080;
 
-fs.writeFile("../../user/server.config.json", json.stringify(settings, null, 2), function writeJSON(err) {
+fs.writeFile("../../user/server.config.json", JSON.stringify(settings, null, 2), function writeJSON(err) {
   if (err) return console.log(err);
 });
 
