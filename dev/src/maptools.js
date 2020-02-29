107419f087cf1eba6ad2bd13c36e2e14245b18c2
Merijn Hendriks <innohurrytocode@gmail.com>
2020-02-29 17:15:51 +0100

    [WIP] work dump

--- dev/src/maptools.js
+++ dev/src/maptools.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const json = require("./json.js");
 
-const inputDir = "input/locations/";
+const inputDir = "input/maps/";
 const outputDir = "output/locations/";
 
 function getDirList(path) {
