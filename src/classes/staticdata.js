23827b370f101ddf01b7db47b7e66e607b87fb9e
Merijn Hendriks <innohurrytocode@gmail.com>
2020-03-10 12:27:10 +0100

    Trying to remove global state

--- src/classes/staticdata.js
+++ src/classes/staticdata.js
@@ -1,11 +1,32 @@
 "use strict";
 
+let items = undefined;
+let globals = undefined;
+let templates = undefined;
+
 function initialize() {
-    global.items = json.parse(json.read(db.user.cache.items));
-    global.globals = json.parse(json.read(db.globals));
-    global.templates = json.parse(json.read(db.user.cache.templates));
     global.tplLookup = itm_hf.createLookup();
     global.gameplayConfig = json.parse(json.read(db.user.configs.gameplay));
+    
+    items = json.parse(json.read(db.user.cache.items));
+    globals = json.parse(json.read(db.globals));
+    templates = json.parse(json.read(db.user.cache.templates));
+}
+
+function getItems() {
+    return items;
+}
+
+function getGlobals() {
+    globals.time = Math.round(Date.now() / 1000);
+    return globals;
+}
+
+function getTemplates() {
+    return templates;
 }
 
 module.exports.initialize = initialize;
+module.exports.getItems = getItems;
+module.exports.getGlobals = getGlobals;
+module.exports.getTemplates = getTemplates;
--- src/responses/staticdata.js
+++ src/responses/staticdata.js
@@ -1,16 +1,15 @@
 "use strict";
 
 function getItems(url, info, sessionID) {
-    return json.stringify({"err": 0, "errmsg": null, "data": items});
+    return json.stringify({"err": 0, "errmsg": null, "data": staticdata_f.getItems()});
 }
 
 function getGlobals(url, info, sessionID) {
-    globals.time = Math.round(Date.now() / 1000);
-    return json.stringify({"err": 0, "errmsg": null, "data": globals});
+    return json.stringify({"err": 0, "errmsg": null, "data": staticdata_f.getGlobals()});
 }
 
 function getTemplates(url, info, sessionID) {
-    return json.stringify({"err": 0, "errmsg": null, "data": templates});
+    return json.stringify({"err": 0, "errmsg": null, "data": staticdata_f.getTemplates()});
 }
 
 router.addStaticRoute("/client/items", getItems);
